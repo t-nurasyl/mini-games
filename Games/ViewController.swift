8f46b657e6f44baa75545177a41c7dd366ad1a2d
Nurasyl Toleukadyr <t.nurassyl@gmail.com>
2018-06-29 11:57:05 +0600

    Initial Commit

---
+++ Games/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Games
+//
+//  Created by Нурасыл on 29.06.2018.
+//  Copyright © 2018 Нурасыл. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
